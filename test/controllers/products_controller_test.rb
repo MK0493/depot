8a74275f7c033778a6f3f3815a38454d1a380297
MK0493 <ashhad0493@gmail.com>
2024-03-05 22:35:10 +0100

    validation

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,7 +3,28 @@ require "test_helper"
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Book #{rand(1000)}"
   end
+
+
+  def create
+    @product = Product.new(product_params)
+      respond_to do |format|
+        if @product.save
+          format.html { redirect_to @product,
+            notice: "Product was successfully created." }
+          format.json { render :show, status: :created,
+            location: @product }
+        else
+          puts @product.errors.full_messages
+          format.html { render :new,
+            status: :unprocessable_entity }
+          format.json { render json: @product.errors,
+            status: :unprocessable_entity }
+        end
+      end
+  end
+  
 
   test "should get index" do
     get products_url
@@ -17,7 +38,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference("Product.count") do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +55,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
   end
 
