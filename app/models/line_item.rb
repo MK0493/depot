1dd0f7b6e7d8a64bf5e47575d7b726abcdc61942
MK0493 <ashhad0493@gmail.com>
2024-03-10 14:05:36 +0100

    upgrading the cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,14 @@
 class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
+
+  # def total_price
+  #   line_items.sum { |item| item.total_price }
+  # end 
+  
+  
 end
