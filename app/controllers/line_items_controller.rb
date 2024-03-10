267da515c8fe729420af02143cb954a3ab83e79f
MK0493 <ashhad0493@gmail.com>
2024-03-11 00:14:39 +0100

    Hotwiring Application

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,14 +26,18 @@ class LineItemsController < ApplicationController
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
  
-
     respond_to do |format|
+      
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart) }
+        format.turbo_stream { @current_item = @line_item }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
+      
       else
+      
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      
       end
     end
   end
